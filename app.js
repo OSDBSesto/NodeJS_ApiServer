48359c2c513d83627e1a088113f434208a25784e
MattiaPoglianiZBook <mattia.pogliani@gmail.com>
2021-03-23 10:08:40 +0100

    add Cors

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
-var http = require("http");
-
 var express = require("express");
+var cors = require("cors");
 var apiServer = express();
 
 var port = 3000;
+
+apiServer.use(cors());
 
 apiServer.listen(port, () => {
     console.log("server running on port ", port);
 });
 
-apiServer.get("/nome", (req, res) =>{
+apiServer.get("/nome", (req, res) => {
     console.log("ho ricevuto una get su nome");
     res.send("ciao, il nome del server è: NODEPOGLIANI");
 });
 
 apiServer.get("/somma", (req, res) => {
-    console.log("richiesta: " , req.query);
-    if(req.query.a && req.query.b){
+    console.log("richiesta: ", req.query);
+    if (req.query.a && req.query.b) {
         var ris = req.query.a - (- req.query.b); // -> il + concatena, quindi sottraggo l'inverso
-        console.log("ris = " , ris);
-        res.send("{ \"risultato\" : " + ris + "}");
-    }else
-    res.send("parametri non corretti");
+        console.log("ris = ", ris);
+        var resObj = { "risultato": ris };
+
+        res.status(200);
+        res.send(resObj);
+    } else {
+        res.status(500);
+        res.send("parametri non corretti");
+    }
+
 });
 
-apiServer.get("/", function(req, res){
+apiServer.get("/", function (req, res) {
     res.send("sei in home");
 });
